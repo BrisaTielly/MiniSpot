ea97ae1eac07f2f7cc86defee4ef063a9070edc3
BrisaTielly <brisatielly12@gmail.com>
2025-10-10 21:36:04 -0300

    Revert "Adiciona classes de: Album, Artist, Track, NormalTrack, FavoriteTrack"
    This reverts commit 44c248498c810921bfbc43506532ee62cd2c8588.

--- src/main/java/com/example/Album.java
+++ src/main/java/com/example/Album.java
@@ -1,71 +1,23 @@
 package com.example;
 
+import java.time.Year;
 import java.util.List;
 
-//Atributos: id, nome, artista, ano, listaFaixas
-//Métodos: exibirFaixas()
-//CRUD: criar, listar, atualizar, excluir
-//POO: Composição (contém lista de faixas); associação com Artista
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Album {
-    private Long id;
-    private String name;
-    private String artist;
-    private int year;
-    private List<Track> trackList;
+    private long id;
+    private String nome;
+    private Artista artista;
+    private Year ano;
+    private List<Faixa> listaFaixas;
 
-    public Album (){
-
-    }
-
-    public Album(Long id, String name, String artist, int year, List<Track> trackList) {
-        this.id = id;
-        this.name = name;
-        this.artist = artist;
-        this.year = year;
-        this.trackList = trackList;
-    }
-
-    public List<Track> displayTracks(){
-        return this.trackList;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getArtist() {
-        return artist;
-    }
-
-    public void setArtist(String artist) {
-        this.artist = artist;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public List<Track> getTrackList() {
-        return trackList;
-    }
-
-    public void setTrackList(List<Track> trackList) {
-        this.trackList = trackList;
-    }
+    public void exibirFaixas(){}
 }
